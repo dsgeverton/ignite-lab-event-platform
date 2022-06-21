ce39c1ddadef8233993ffd5c132319dc70754aa7
Everton <everton@pop-os.localdomain>
2022-06-21 20:54:03 -0300

    finish Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { Header } from "./components/Header"
-import { Lesson } from "./components/Lesson"
-import { Player } from "./components/Player"
-import { Sidebar } from "./components/Sidebar"
+import { Event } from "./pages/Event"
 
 function App() {
 
   return (
     <div>
-      <Header />
-      <Sidebar />
-      <Player />
-      <Lesson />
+      <Event />
     </div>
     )
 }
